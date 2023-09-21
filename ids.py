d31132c016fde7620e063fbdfef4b3230569aa86
Richard Hu <neverlight.hu@gmail.com>
2023-09-21 16:05:15 -0400

    bug fix

--- ids.py
+++ ids.py
@@ -1,28 +1,29 @@
 from puzzle import Puzzle
 
-def dls(puzzle, limit):
+def dls(puzzle, limit, nodes_generated):
     visited = set()
     stack = [(puzzle, 0)]
-    nodes_generated  = 0
 
     while stack:
         current_puzzle, current_depth = stack.pop()
 
         if current_puzzle.solved():
-            ebf = nodes_generated**(1/current_depth)
-            return current_depth, nodes_generated,ebf
-        
+            ebf = nodes_generated ** (1 / current_depth) if current_depth != 0 else 0
+            return current_depth, nodes_generated, ebf
+
         visited.add(str(current_puzzle.board))
 
         if current_depth < limit:
-            for puzzle in current_puzzle.neighbors():
-                if str(puzzle.board) not in visited:
-                    nodes_generated +=1
-                    stack.append((puzzle, current_depth+1))
+            for neighbor_puzzle in current_puzzle.neighbors():
+                if str(neighbor_puzzle.board) not in visited:
+                    nodes_generated += 1
+                    stack.append((neighbor_puzzle, current_depth + 1))
+
+    return None, nodes_generated, None
 
 def ids(start_state, depth_limit):
-    for limit in range(depth_limit +1):
-        result = dls(start_state, limit)
-        if result:
-            return result
-        
+    nodes_generated = 0  # Initialize nodes_generated
+    for limit in range(depth_limit + 1):
+        depth, nodes_generated, ebf = dls(start_state, limit, nodes_generated)  # Update nodes_generated
+        if depth is not None:
+            return depth, nodes_generated, ebf
